cfdffbee64b756bccf1f9dfbc4bfa2d682100a92
zerus2g <tuanthuong3001@gmail.com>
2024-12-19 21:41:34 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: ` Nếu em hỏi anh về tình yêu thì anh không trả lời được nhưng nếu em hỏi cô gái anh yêu là ai thì đó chính là em đấy! `,
+    btnIntro: 'hihi',
+    title: 'Anh đã yêu vợ ngay từ khi vợ voice cho anh  😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc gặp được em ',
+    btnYes: 'em siêuuuu yêu anh <33',
+    btnNo: 'Không, em hông yêu anh :3',
+    question:'Em có yêu anh hôngg <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'tại anh đẹp trai , giỏi , ga lăng , blabla <33333333',
+    mess: 'hihi ngại quá 🥰. Yêu vợ nhiều nhiều 😘😘',
+    messDesc: 'Anh yêu em, chúng ta bên nhau tới già nhoa <3',
+    btnAccept: 'ehehehe <3',
+    messLink: 'https://www.facebook.com/momo0849102558/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
